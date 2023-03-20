dba8acb2259ccf55b8f677f588286b1f1c91c68c
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-20 20:11:27 +0800

    set darkMode to class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // support toggling dark mode manually instead of 
+  // relying on the operating system preference
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
