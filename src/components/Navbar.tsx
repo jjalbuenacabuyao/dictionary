10d4e7e3b9a491f3fde35244d610a7a44717df65
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-20 14:50:24 +0800

    created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaGithub } from "react-icons/fa"
+import { FiBook } from "react-icons/fi"
 
-type Props = {}
+const Navbar = () => {
 
-const Navbar = (props: Props) => {
   return (
-    <div>Navbar</div>
+    <header>
+      <nav>
+        <ul className="flex justify-between items-center">
+          <li><a href="#" className='flex items-center'><FiBook size="2.5rem" /> Dictionary</a></li>
+          <li><a href="https://github.com/jjalbuenacabuyao/dictionary" target='_blank'><FaGithub size="1.5rem" /></a></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
