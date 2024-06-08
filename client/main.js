e49eab8a776f46ceda295eecf109fbded7e110d5
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-06-08 11:16:48 +0800

    add event listener to the button to change the theme

---
+++ client/main.js
@@ -0,0 +1,12 @@
+const themeToggler = document.getElementById("theme-toggler")
+const body = document.querySelector("body")
+
+themeToggler.addEventListener("click", () => {
+  const currentTheme = body.getAttribute("data-theme")
+
+  if (currentTheme === "light") {
+    body.setAttribute("data-theme", "dark");
+  } else {
+    body.setAttribute("data-theme", "light");
+  }
+})
