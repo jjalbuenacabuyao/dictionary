5ce8888c7b0ba2795614ca60eaf1c7a2810f02d3
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-06-23 20:36:30 +0800

    remove the logic for audioPlayer

--- static/main.js
+++ static/main.js
@@ -1,18 +1,12 @@
-const themeToggler = document.getElementById("theme-toggler")
-const body = document.querySelector("body")
+const themeToggler = document.getElementById("theme-toggler");
+const body = document.querySelector("body");
 
 themeToggler.addEventListener("click", () => {
-  const currentTheme = body.getAttribute("data-theme")
+  const currentTheme = body.getAttribute("data-theme");
 
   if (currentTheme === "light") {
     body.setAttribute("data-theme", "dark");
   } else {
     body.setAttribute("data-theme", "light");
   }
-})
-
-const audioPlayer = document.querySelector("[data-audio-player]");
-const pronounciationAudio = document.querySelector("[data-pronounciation]");
-audioPlayer.addEventListener("click", () => {
-  pronounciationAudio.play();
-})
+});
